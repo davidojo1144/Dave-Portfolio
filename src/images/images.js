ec607a8b33d6c0eb8dfe2efc2ec4c6f072e7ed5e
davidojo1144 <davco1144@gmail.com>
2025-08-28 15:30:01 +0100

    Dave Portfolio

--- src/images/images.js
+++ src/images/images.js
@@ -1,8 +1,10 @@
 import project1 from "./project-1.png"
 import project2 from "./project-2.png"
+import project3 from "./project-3.png"
 
 
 export const assets = {
     project1,
-    project2
+    project2,
+    project3
 }
